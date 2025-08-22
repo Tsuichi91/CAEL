851b1513f48308cc06ab6330cb99872417b37466
Tsuichi91 <Tsukatchi@gmail.com>
2025-08-22 13:12:42 +0200

    Switch to EN

--- assets/js/timeline-data.js
+++ assets/js/timeline-data.js
@@ -1,13 +1,13 @@
 
 window.CAEL_TL_BASE = [
-  {date:'2005-02-18', title:'Geburt',            type:'Milestone', note:'Geboren in Seoul, Südkorea.'},
-  {date:'2019-06-10', title:'Trainee Start',     type:'Milestone', note:'Beginn des Trainings.'},
-  {date:'2023-08-04', title:'Debüt als CΛEL',    type:'Debut',     note:'Künstlerdebüt/Identität enthüllt.'},
-  {date:'2023-08-18', title:'VEINS',             type:'Single',    note:'Debüt‑Single.'},
-  {date:'2024-02-16', title:'D I V I D E',       type:'Mini',      note:'Birthday‑Week Comeback.'},
-  {date:'2024-10-18', title:'NIGHT DRIVE',       type:'Single',    note:'Neon Night.'},
-  {date:'2025-05-23', title:'ICON (Pre‑Release)',type:'Single',    note:'Vorab‑Single fürs Album.'},
-  {date:'2025-06-06', title:'PARADE (Pre‑MV)',   type:'MV',        note:'Teaser‑MV.'},
-  {date:'2025-06-13', title:'HALO // HAVOC',     type:'Album',     note:'Editorial Prism/Holo.'},
-  {date:'2025-08-01', title:'CITY CHORDS',       type:'Mini',      note:'City‑Pop‑Rock.'}
+  {date:'2000-02-18', title:'Geburt',            type:'Milestone', note:'Geboren in Seoul, Südkorea.'},
+  {date:'2014-06-10', title:'Trainee Start',     type:'Milestone', note:'Beginn des Trainings.'},
+  {date:'2019-08-04', title:'Debüt als CΛEL',    type:'Debut',     note:'Künstlerdebüt/Identität enthüllt.'},
+  {date:'2019-08-18', title:'VEINS',             type:'Single',    note:'Debüt‑Single.'},
+  {date:'2020-02-16', title:'D I V I D E',       type:'Mini',      note:'Birthday‑Week Comeback.'},
+  {date:'2021-10-18', title:'NIGHT DRIVE',       type:'Single',    note:'Neon Night.'},
+  {date:'2022-05-23', title:'ICON (Pre‑Release)',type:'Single',    note:'Vorab‑Single fürs Album.'},
+  {date:'2022-06-06', title:'PARADE (Pre‑MV)',   type:'MV',        note:'Teaser‑MV.'},
+  {date:'2022-06-13', title:'HALO // HAVOC',     type:'Album',     note:'Editorial Prism/Holo.'},
+  {date:'2023-02-01', title:'CITY CHORDS',       type:'Mini',      note:'City‑Pop‑Rock.'}
 ];
